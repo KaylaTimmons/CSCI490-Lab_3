25565c97e78f05a73e7f67d35c818efe31176cf2
“Kayla <timmonskc@g.cofc.edu>
2021-03-11 19:03:59 -0500

    Not Done Yet

--- app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    pivate listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        schedule = (ListView) this.findViewById(R.id.class_schedule);
+        String[] classes = {"CSCI 114", "CSCI 230", "CSCI 332", "CSCI 370", "MUSC 382", "CSCI 392"};
+
+        ArrayAdapter adapterr = new ArrayAdapter(this, android.R.layout.simple_list_item_1, classes);
+
+        schedule
+
     }
 }
